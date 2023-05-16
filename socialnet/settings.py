d206b9ca6ea39ceef05ac475a3837660efb33eab
uzoma chimdindu precious <uzomachimdindu300@gmail.com>
2023-05-16 11:46:56 +0100

    first commit

--- socialnet/settings.py
+++ socialnet/settings.py
@@ -75,11 +75,6 @@ if DEBUG:
    EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'uzomachimdindu300@gmail.com'
-EMAIL_HOST_PASSWORD =''
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
