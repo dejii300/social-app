8bcafdb87e82607e35d5afff6e61d8d9638c9ef8
uzoma chimdindu precious <uzomachimdindu300@gmail.com>
2023-05-16 21:30:07 +0100

    first commit

--- socialnet/settings.py
+++ socialnet/settings.py
@@ -164,7 +164,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    (os.path.join(BASE_DIR, 'static_project', ))
+    (os.path.join(BASE_DIR, 'static_project', 'static_cdn'))
 ]
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'socialnet', 'static_cdn', 'static_root')
